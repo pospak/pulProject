7970dc64e4b429ea71ceba648c04d8c1fdec294b
pospak <pospk65@gmail.com>
2024-05-24 11:03:59 +0200

    add footer

---
+++ www/footer.php
@@ -0,0 +1,15 @@
+<div class="bg">
+<div class="footer">
+        <div class="social-links">
+            
+         
+            <a href="https://www.facebook.com/michal.pospisil.3745" class="social-icon"><img  width="40px" height="40px" src="facebook-icon.png" alt="Facebook"></a>
+            <a href="https://www.youtube.com/@mimajspo_create_original" class="social-icon"><img width="50px" height="50px" src="twitter-icon.png" alt="Youtube"></a>
+            <a href="https://instagram.com/pospak65?igshid=MzMyNGUyNmU2YQ==" class="social-icon"><img width="50px" height="50px" src="ig.png" alt="Instagram"></a>
+            
+        </div>
+      <!--   <div class="footer-text">
+           &copy; #citrakIT3 2024 site by: POSPÁK65
+        </div> -->
+    </div>
+</div>
