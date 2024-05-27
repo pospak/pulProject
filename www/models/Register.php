594545a0fd2bb1b080d47628e7368918d8d16475
Michal Pospíšil <pospk65@gmail.com>
2024-05-27 20:39:53 +0200

    register update

--- www/models/Register.php
+++ www/models/Register.php
@@ -4,10 +4,9 @@ class Register{
     public static function PospakTube($username, $password, $email) {
         $connection = Database::getConnection();
         $password = password_hash($password, PASSWORD_DEFAULT);
-        $sql = "INSERT INTO PospakTubeUsers (username, password, email, profilePhotom, targetID) VALUES (?, ?, ?, ?, ?)";
+        $sql = "INSERT INTO PospakTubeUsers (targetID, username, password, email) VALUES (?, ?, ?, ?)";
         $stmt = $connection->prepare($sql);
-        $profilePhoto = "/images/profilePhotos/logo.png";
-        $stmt->bind_param("ssssi", $username, $password, $email,$profilePhoto, 552);
+        $stmt->bind_param("sssi", $username, $password, $email,552);
         $stmt->execute();
         return true;
     }
