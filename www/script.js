56d2abce55d0f7230edfaf86ff26e40d1735f86d
pospak <pospk65@gmail.com>
2024-05-27 10:07:03 +0200

    hmm

--- www/script.js
+++ www/script.js
@@ -12,22 +12,21 @@ form.addEventListener('input', () => {
     submitButton.disabled = isAnyInputEmpty;
 
     // Apply different styles to the submit button based on its disabled status
-
+    if (submitButton.disabled) {
+        submitButton.style.textDecoration="none";
+        submitButton.style.color = "white";
+        submitButton.style.background = "transparent";
+        submitButton.style.border = "none";
+    } else {
+    submitButton.style.width= "100%";
+    submitButton.style.padding= "10px";
+    submitButton.style.margin = "10px 0";
+    submitButton.style.borderRadius=  "5px";
+    submitButton.style.border = "1px solid #02f869";
+    submitButton.style.backgroundColor = "#02f869";
+    submitButton.style.color = "white";
+    submitButton.style.cursor = "pointer";
+    
+    }
 });
 
-if (submitButton.disabled) {
-    submitButton.style.textDecoration="none";
-    submitButton.style.color = "white";
-    submitButton.style.background = "transparent";
-    submitButton.style.border = "none";
-} else {
-submitButton.style.width= "100%";
-submitButton.style.padding= "10px";
-submitButton.style.margin = "10px 0";
-submitButton.style.borderRadius=  "5px";
-submitButton.style.border = "1px solid #02f869";
-submitButton.style.backgroundColor = "#02f869";
-submitButton.style.color = "white";
-submitButton.style.cursor = "pointer";
-
-}
