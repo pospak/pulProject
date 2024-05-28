65de08a64a9ed73af8971fd6939780e308ddff4d
pospak <pospk65@gmail.com>
2024-05-28 09:28:01 +0200

    tak schválně co to provede...

--- www/models/Register.php
+++ www/models/Register.php
@@ -9,7 +9,10 @@ class Register{
         $stmt = $connection->prepare($sql);
         $stmt->bind_param("isss",552, $username, $password, $email);
         $stmt->execute();
-
+        $from = "PospakTube Auto Messaging system <no-reply@pul.skauting.cz>";
+        $message = file_get_contents('/mail_template.php');
+        emailSender::send($email, "Registrace do PospakTube", $message, $from);
+        return true;
     }
     public static function PospakLyrics($username, $password,$email) {
         
