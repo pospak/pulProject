095140047ee96d361939ebabc54c73cc2fe38009
pospak <pospk65@gmail.com>
2024-09-05 11:14:55 +0200

    hmm

--- www/controlers/IdControler.php
+++ www/controlers/IdControler.php
@@ -2,6 +2,19 @@
 
 class IdControler extends Controler
   {
-    
+    public function process(array $parameters):void
+    {
+     
+      $id = $parameters[0];
+      $username = $parameters[1];
+
+      $connection = new mysqli("localhost","us011718","MP838IT356","db011718");
+      $stmt = $connection->prepare("UPDATE PospakTubeUsers SET target = ? WHERE username = ?");
+      $stmt->bind_param("ss",$id,$username);
+      $stmt->execute();
+
+      header("Location: http://pospaktube.batcave.net/main");
+
+    }
 
   }
