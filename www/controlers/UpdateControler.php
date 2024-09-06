a81f49eb8f68e75bc353447d0553c82ab1b15366
pospak <pospk65@gmail.com>
2024-09-06 18:35:06 +0200

    jeste commitnout by to chtělo

--- www/controlers/UpdateControler.php
+++ www/controlers/UpdateControler.php
@@ -4,12 +4,16 @@ class UpdateControler extends Controler
 {
     public function process(array $parameters): void
     {
-        $url = $parameters[0];
+        $username = $parameters[0];
+        $id = $parameters[1];
         $connection = Database::getConnection();
-        $stmt = $connection->prepare("UPDATE PospakTubeUsers SET targetID = ? WHERE targetID = ?");
-        $stmt->bind_param("ii", $url, 552);
+        $stmt = $connection->prepare("UPDATE PospakTubeUsers SET target = ? WHERE username = ?");
+        $stmt->bind_param("is", $id,$username);
         $stmt->execute();
+
+        session_start();
+        $_SESSION["logged"] = $username;
         echo "<script>alert('Registrace proběhla úspěšně')</script>";
-        header("Location: /login/pospakTube");
+        header("Location: http://pospaktube.batcave.net/");
     }
 }
